e61fd71c27ede2fa2deca95b5b554c47e2a67f87
CarolinaMedin <carolina.medina@usach.cl>
2020-01-18 18:48:01 -0300

    prueba "Mensaje de Bienvenida" exitosa

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
-
-
-
-// /*Mensaje de Bienvenida*/
-// const svgPath = document.querySelectorAll('.path');
-
-// // Creamos la animación del Mensaje de Bienvenida
-// let Animacion_de_Bienvenida = anime({		// Objeto que contiene los detalles de la animación
-//   targets: svgPath,						// Referencia a los elementos que se queiren animar
-//   loop: true,
-//   direction: 'alternate',
-//   strokeDashoffset: [anime.setDashoffset, 0],
-//   easing: 'easeInOutSine',
-//   duration: 700,
-//   delay: (el, i) => { return i * 500 }
-// });
-
-
 function main(){
 	// console.log("main")
 
 
 	// Ejecutamos el objeto anime después de cargar la página
-	var lineDrawing = anime({
-	  targets: '#lineDrawing .lines path',
-	  strokeDashoffset: [anime.setDashoffset, 0],
-	  easing: 'easeInOutSine',
-	  duration: 3000,
-	  delay: function(el, i) { return i * 250 },
+	// Creamos la animación del Mensaje de Bienvenida
+	let animation = anime({
+	  targets: '.Bienvenida',
+	  opacity: 1,
+	  translateY: 50, 
+	  rotate: {
+	    value: 360,
+	    duration: 2000,
+	    easing: 'easeInExpo'
+	  }, 
+	  scale: anime.stagger([0.7, 1], {from: 'center'}), 
+	  delay: anime.stagger(100, {start: 1000}), 
+	  translateX: [-10, 30],
 	  direction: 'alternate',
 	  loop: true
-	});
+	}); 
 
 }
 
